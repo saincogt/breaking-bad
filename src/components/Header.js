7d06e5be20c025b9502b654491a0d03c76eff2e3
saincogt <saincogt@gmail.com>
2020-07-14 21:21:53 +0930

    initial commits

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../img/logo.png';
+
+const Header = () => (
+	<header className='center'>
+		<img src={logo} alt='logo' />
+	</header>
+);
+
+export default Header;
