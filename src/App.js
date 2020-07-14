93e0e3e4497a56625610148f608b0abb5036135e
saincogt <saincogt@gmail.com>
2020-07-14 21:48:39 +0930

    add spinner

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import axios from 'axios';
 const App = () => {
 	const [items, setItems] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [query, setQuery] = useState('');
 	useEffect(() => {
 		const fetchItems = async () => {
 			const result = await axios(
-				`https://breakingbadapi.com/api/characters`
+				`https://breakingbadapi.com/api/characters?name=${query}`
 			);
 			setItems(result.data);
 			setIsLoading(false);
 		};
 		fetchItems();
-	}, []);
+	}, [query]);
 	return (
 		<div className='container'>
 			<Header />
-			<Search />
+			<Search getQuery={(q) => setQuery(q)} />
 			<CharacterGrid isLoading={isLoading} items={items} />
 		</div>
 	);
